b20b5f7746350867de5179f0f694f1bc525af9bd
telka <sundays.lake@gmail.com>
2025-12-07 13:31:04 +0900

    STEP3-⓼

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 
 android {
-    namespace = "com.example.boatracerbook"
+    namespace = "com.example.wrmmo2"
     compileSdk = 36
     ndkVersion = "27.0.12077973"
     // ndkVersion = flutter.ndkVersion
@@ -32,7 +32,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.boatracerbook"
+        applicationId = "com.example.wrmmo2"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
